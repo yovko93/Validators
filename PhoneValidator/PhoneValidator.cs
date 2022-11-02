d2bb17a9a41171f3c36fec7d14be161b2d7477e2
Yovko Garkov <yovko.garkov@elando.com>
2022-11-02 16:00:05 +0200

    Add phone validator

---
+++ PhoneValidator/PhoneValidator.cs
@@ -0,0 +1,37 @@
+﻿namespace Validators.PhoneValidator
+{
+    #region Usings
+    using System.Text.RegularExpressions;
+    #endregion
+
+    public class PhoneValidator
+    {
+        public bool IsValid(string phoneNumber)
+        {
+            // 1. Remove whitespaces and letters
+            phoneNumber = ReturnDigits(phoneNumber);
+
+            // 2. Check PhoneNumber length
+            if (phoneNumber.Length == 9)
+            {
+                return HandleValidation(phoneNumber);
+            }
+
+            return false;
+        }
+
+        private bool HandleValidation(string phoneNumber)
+        {
+            string PATTERN = @"^[8]{1}[7-9]{1}[2-9]{1}[0-9]{6}$";
+            var isMatch = Regex.IsMatch(phoneNumber, PATTERN);
+            return isMatch;
+        }
+
+        #region Helpers
+        private string ReturnDigits(string str)
+        {
+            return Regex.Replace(str, @"\D+", string.Empty);
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Validators.PhoneValidator;
+
+var validator = new PhoneValidator();
+
+while (true)
+{
+    Console.WriteLine("Insert PhoneNumber: (xxx or xxx):");
+    string? input = Console.ReadLine();
+
+    try
+    {
+        bool phoneInfo = validator.IsValid(input);
+        Console.WriteLine(phoneInfo);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
